fixed result dimensionality and the sign of error impoved documentation and help messages fixed pores.py benchmarking

fitpore.py
@@ -14,16 +14,18 @@ from scipy.stats import linregress
 parser = argparse.ArgumentParser(description='Get pore tension in 2 steps.',
         epilog="""First run with only a filename as input and remember the boundaries of the linear stage.
         Then run again supplying all arguments to get the linear region fitted and pore tension displayed.""")
-parser.add_argument('filename', help='Name of the input file with pore radii and frame numbers')
-parser.add_argument('-s', type=int, default=0, help='Start of the linear region')
-parser.add_argument('-e', type=int, default=0, help='End of the linear region')
+parser.add_argument('filename', help='Name of the input text file with pore radii and frame numbers')
+parser.add_argument('-s', type=int, default=0, help='Start of the linear region (frame number)')
+parser.add_argument('-e', type=int, default=0, help='End of the linear region (frame number)')
 parser.add_argument('-r', type=float, help='Initial radius of the vesicle in microns.')
 parser.add_argument('-f', type=float, help='Speed of image acquisition in frames per second')
 parser.add_argument('-v', type=float, default=1.0e-3, 
-                    help='Viscosity of the bulk solution in Pa*s (defaults to water)')
+                    help='Viscosity of the bulk solution in Pa*s (defaults 1e-3 Pa*s for water)')
 
+if len(sys.argv)==1:
+        parser.print_help()
+        sys.exit(1)
 args = parser.parse_args()
-print args
 
 radius, frame = np.loadtxt(args.filename, unpack=1, usecols=(0,5))
 lnr = np.log(radius)
@@ -73,11 +75,12 @@ else:
     plt.xlabel('frame number')
     plt.ylabel('$\\ln (r_p)$')
     
-    #-1.5e-12 = -3/2 * (1.e-6 m)^2 since args.r is in microns
-    gamma = -1.5e-12 * np.pi * args.v * args.r*args.r * args.f * a
-    gammastd = -1.5e-12 * np.pi * args.v * args.r*args.r * args.f *std
+    modelgamma = lambda x: -1.5 * np.pi * args.v * args.r*args.r * args.f *x
+    gamma =  modelgamma(a)
+    gammastd = np.abs(modelgamma(std))
     
-    plt.title('$\\gamma$ = %e $\\pm$ %e, from %i to %i'%(gamma, gammastd, lim1, lim2))
+    #since R is in micrometers and there is R**2, gamma is in picoNewtons
+    plt.title('$\\gamma$ = %f $\\pm$ %f pN, from %i to %i'%(gamma, gammastd, lim1, lim2))
     plt.show()
     
 

pores.py
@@ -108,17 +108,26 @@ def process_image(filename, nskip):
     np.savetxt(nameout, out, fmt='%.7e')
     
 if __name__ == '__main__':
+    import sys
     import argparse
-    parser = argparse.ArgumentParser(description = 'Extract pore positions and radii.')
+    parser = argparse.ArgumentParser(description = 'Extract pore positions and radii.',
+                    epilog="""Outputs TSV text file named imagename_skipXXX.txt, with 6 columns:
+                                    pore radius in pixels, 
+                                    4 columns for x and y coordinates of pore edges, 
+                                    corresponding frame number.""")
     parser.add_argument('filename', help="Name of the multipage b/w tiff file")
     parser.add_argument('--skip', default=1, type=int, help='Analyse only every SKIPth frame (default is every frame)')
     parser.add_argument('--test', type=int, default=0, help='Run the procedure TEST times and report the minimal of them')
+    
+    if len(sys.argv)==1:
+        parser.print_help()
+        sys.exit(1)
     args = parser.parse_args()
     
-    if args.t > 0:
+    if args.test > 0:
         import timeit
         print min(timeit.repeat("process_image('%s', %i)"%(args.filename, args.skip), 
                                 "from __main__ import process_image", 
-                                repeat=args.t, number=1))
+                                repeat=args.test, number=1))
     else:
         process_image(args.filename, args.skip)