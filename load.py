9784ffba53f5b30a1163244deb87701992be87da
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2012-01-02 14:18:24 +0100

    switched to TIFF implementation of PIL implemeted pore edges and pore radius finding BUG: results do not coinside with matlab and are considerably larger

--- load.py
+++ load.py
@@ -5,9 +5,46 @@ Created on Thu Dec 15 00:31:09 2011
 @author: Pavlo Shchelokovskyy
 """
 
-from tifffile import TIFFfile
+from PIL import Image
+
+#class TIFFImage(Image.Image):
+#    """"""
+#    def __init__(self, *args, **kwargs):
+#        Image.Image.__init__(self, *args, **kwargs)
+#        self._nofframes = None
+#        
+#    def _countframes(self):
+#        count = 0
+#        while True:
+#            try:
+#                self.seek(count)
+#            except EOFError:
+#                return count
+#            else:
+#                count += 1
+#    
+#    def __len__(self):
+#        if self._nofframes:
+#            return self._nofframes
+#        else:
+#            self._nofframes = self._countframes
+
+
+def count_tiff_frames(tiffimage):
+    count = 0
+    while True:
+        try:
+            tiffimage.seek(count)
+        except EOFError:
+            return count
+        else:
+            count += 1
+
+
 
 def load_images(filename):
     """Loads images from multipage TIFF"""
-    tif = TIFFfile(filename)
+    tif = Image.open(filename)
+    size = count_tiff_frames(tif)
+    return tif, size
     
--- pores.py
+++ pores.py
@@ -3,170 +3,58 @@
 Created on Thu Dec 15 00:26:39 2011
 
 @author: Pavlo Shchelokovskyy
-
-comments machine translated from French and not critically analyzed
-are marked with FR:
-
-These are the steps for a single frame from a single image:    
-# Find center of the image
-# Blacken the left half of the image - that leaves either two separated 
-  arc-like clusters or a single big hemi-circular one.
-# Find indices (i.e. coordinates) of all non-zero elements
-# for all nonzero elements find an angle between the element, center of image 
-  and horizontal right (+) direction
-# for nonzero elements in upper-right quadrant take element and its position 
-  with the minimal angle
-# for nonzero elements in lower-right quadrant take element and its position 
-  with the maximal angle
-# find distance between these two points. if it is (one or zero?) - 
-  there is no pore, otherwise it is a pore diameter
 """
+from __future__ import division
 
 import numpy as np
-
-from load import load
-
-def find_pore_radius(img):
-    """Finds a vesicle pore edges in a binary image
-
-The binary image of the porated vesicle has to be rotated and centered such as 
-that the pore is always to the right and the horizontal midsection of the image 
-always goes through the pore if ther is one.
-    
-"""
-    
-    
-def pores(njump=100):
+from scipy.misc import fromimage
+ 
+def pore_edges(imagestack, nofimages, njump=1):
     """Computes pore sizes from binary vesicle images
 
-argin: 
+Images must be rotated with the pore to the right and centered
+
+Input: 
 - njump : interval between images that are considered (100 by default, 
           meaning that pore size is computed for slices 1, 101, 201, 301...)
 
 """
-    #load parameters
-#==============================================================================
-#     names,nslices,Tacq,pixel_size,numero_manip,R,td,tf = load()
-#==============================================================================
-    #names - list of names of multipage TIFFs
-    #nslices - corresponding number of images in each of multipage TIFFs
-    #Tacq - timestep between frames (microsec)
-    #pixel_size - physical scale of images (micron/pixel)
-    #numero_manip - general number of vesicle studied 
-    #               (can have several image sets recorded for the same vesicle)
-    #R - radius of the vesicle (micron)
-    #td, tf - lower and upper time limits of linear part of R^2*ln(r) (millisec)
-    nmanips=len(nslices) #?overall number of image stacks studied
+    framenos = range(0, nofimages, njump)    
+    edgestop = np.empty((len(framenos),2))
+    edgesbottom = np.empty_like(edgestop)
+    #looping through images in the multiimage TIFF
+    for i, frameno in enumerate(framenos):
+        imagestack.seek(frameno)
+        image = fromimage(imagestack)
+        
+        #find the approximate center
+        centerx = image.shape[1]//2
+        centery = image.shape[0]//2
+        
+        #extract upper-right quadrant
+        top = image[:centery,centerx:]
+        ytop, xtop = np.nonzero(top)
+        angletop = np.arctan2(centery - ytop, xtop)
+        mintop = np.argmin(angletop)
+        edgestop[i,:] = ytop[mintop], xtop[mintop]+centerx
+        
+        #extract lower-right quadrant
+        bottom = image[centery:,centerx:]
+        ybottom, xbottom = np.nonzero(bottom)
+        anglebottom = np.arctan2(ybottom, xbottom)
+        minbottom = np.argmin(anglebottom)
+        edgesbottom[i,:] = ybottom[minbottom]+centery, xbottom[minbottom]+centerx
     
-    #to start profiling timer
-    #TODO: start profiling timer here
+    return framenos, edgestop, edgesbottom
     
-    #looping through image files (each is a multipage binary TIFF)
-    for imanip in range(nmanips):
-        #FR:shifting a space to the end of the file name
-        pass #some code here
-        
-        #FR:init of the backup file
-        pass #some array init here
-        
-        #looping through images in the multiimage TIFF
-        for islice in range(0, nslices(imanip), njump):
-            #FR:we initialize the variables each time through the loop
-            pass #many various arrays are inited
-            
-            #reading the image
-            #TODO: read one next image from multipage TIFF
-            image = sp.misc.imread(names[imanip]) #temp hack just to define image
-            #TODO: here it looks like expanding the image with 1 pixel black borders in all 4 sides
-            # probably for the case of very tight cropping to ensure the padding
-            
-            #get image size and find the approximate center
-            nl, nc = image.shape()
-            xc = floor(nl/2)
-            yc = floor(nc/2)
-            
-            #FR:remove the lower part of the image
-            im = image
-            im[:,:yc-1] = 0
-            #apparently this removes left part
-            
-            #finds inices of all nonzero elements
-            x,y = np.nonzero(im)
-            
-            #some debug plotting here
-            pass
-            
-            #--------------------
-            # start of analysis
-            #--------------------
-            
-            #NE part
-            #set the starting point
-            x_NE=trouve_depart_NE(xc,yc,im,nl)
-            #FR:init buddylist
-            copains_NE=init_liste_copains(x_NE,yc,im)
-            #FR: build buddylist
-            copains_NE=construit_liste_copains(copains_NE,im)
-            #FR:returns the index of friends with the largest theta
-            z_NE,theta_NE=theta_max_NE(copains_NE,nl,nc,xc,yc)
-            
-            
-            #NO part
-            #set the starting point
-            x_NO=trouve_depart_NO(xc,yc,im);
-            #FR:init buddylist
-            copains_NO=init_liste_copains(x_NO,yc,im);
-            #FR: build buddylist
-            copains_NO=construit_liste_copains(copains_NO,im);
-            #FR:returns the index of friends with the largest theta
-            z_NO,theta_NO=theta_min_NO(copains_NO,nl,nc,xc,yc);
-            
-            #TODO:find coordinates of arc ends
-            #[xt_NE,yt_NE]=ind2sub([nl,nc],z_NE)
-            #[xt_NO,yt_NO]=ind2sub([nl,nc],z_NO)
-
-            #if there is no pore
-            if theta_NE>theta_NO:
-                xt_NE=x_NE
-                yt_NE=yc
-                xt_NO=x_NE
-                yt_NO=yc
-            
-            #TODO:calculate half-distance between arc ends, multiply by pixel size
-            #rpore=0.5*norm([(xt_NO-xt_NE) (yt_NO-yt_NE)]')*pixel_size(imanip)
-            
-            #some debug plotting here
-            pass
-            
-            #--------------------
-            # end of analysis
-            #--------------------
-            
-            #we get the results
-            #sauv=[sauv;[rpore xt_NO yt_NO xt_NE yt_NE islice]]
-        
-        #save results of this image and njump to a file
-        sp.savetxt()
-        
-    #TODO:stop and present the timer
+def pore_radii(edgestop, edgesbottom):
+    """"""
+    #distance between two edges of the pore
+    dist = np.sqrt(np.sum((edgesbottom-edgestop)**2, axis=1))
     
-def trouve_depart_NE(xc,yc,im,nl):
-    pass
-
-def trouve_depart_NO(xc,yc,im):
-    pass
-
-def init_liste_copains(x_NE,yc,im):
-    pass
-
-def ajoute_voisins(x,y,im,cops):
-    pass
-
-def construit_liste_copains(copains_NE,im):
-    pass
-
-def theta_max_NE(copains_NE,nl,nc,xc,yc):
-    pass
-
-def theta_min_NO(copains_NO,nl,nc,xc,yc):
-    pass
+    #if the edges are adjacent on y-coordinate - ther is no pore
+    nopore = (edgesbottom[:,0]-edgestop[:,0] ==1)
+    
+    #calculate pore radius and set to 0 when there is no pore
+    rpores = np.where(nopore, np.zeros_like(dist), dist) / 2
+    return rpores
