95adb60e16832d853a514dd8397d276435c59ead
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2012-01-02 14:22:11 +0100

    improved pore edge position detection - no need to center the pore now results are very much coinsiding with MATLAB now

--- pores.py
+++ pores.py
@@ -30,14 +30,15 @@ def load_imagestack(filename):
     size = count_tiff_frames(tif)
     return tif, size
     
-def pore_edges(imagestack, nofimages, njump=1):
-    """Computes pore sizes from binary vesicle images
+def pores(imagestack, nofimages, njump=1):
+    """Computes pore positions and sizes from binary vesicle images
 
-Images must be rotated with the pore to the right and centered
+Images must be adjusted so that the vesicle approximately centered 
+and the pore is in the right half of the image
 
 Input: 
-- njump : interval between images that are considered (100 by default, 
-          meaning that pore size is computed for slices 1, 101, 201, 301...)
+- njump : interval between images that are analysed (i.e. 100 means 
+  pore size is computed for slices 1, 101, 201, 301...)
 
 """
     framenos = np.arange(0, nofimages, njump)
@@ -54,58 +55,53 @@ Input:
         
         s = np.ones((3,3))#structure defining which directions are taken as neighbours
 
-#==============================================================================
-# Upper-right quadrant
-#==============================================================================
-        #extract upper-right quadrant
-        top = image[:centery,centerx:]
-        #find the innermost point on the left edge of the image
-        innertop = np.max(np.nonzero(top[:,0]))
-        #label interconnected features/clusters
-        toplabeled, noftoplabels = label(top, structure=s)
-        #find which cluster corresponds to innermost point on the left edge
-        #this is the vesicle
-        innertoplabel = toplabeled[innertop, 0]
-        #take coordinates comprising this cluster
-        ytop, xtop = np.nonzero(toplabeled == innertoplabel)
-        #find point with minimal angle
-        angletop = np.arctan2(centery - ytop, xtop)
-        mintop = np.argmin(angletop)
-        #shift coordinates back to frame of the whole image
-        edgestop[i,:] = ytop[mintop], xtop[mintop]+centerx
-#==============================================================================
-# Lower-right quadrant - procedure similar to the upper-right one
-#==============================================================================
-        bottom = image[centery:,centerx:]
-        innerbottom = np.min(np.nonzero(bottom[:,0]))
-        bottomlabeled, nofbottomlabels = label(bottom, structure=s)
-        innerbottomlabel = bottomlabeled[innerbottom, 0]
-        ybottom, xbottom = np.nonzero(bottomlabeled == innerbottomlabel)
-        anglebottom = np.arctan2(ybottom, xbottom)
-        minbottom = np.argmin(anglebottom)
-        edgesbottom[i,:] = ybottom[minbottom]+centery, xbottom[minbottom]+centerx 
-    return framenos+1, edgestop, edgesbottom
-    
-def pore_radii(edgestop, edgesbottom):
-    """Computes radii of pore given its edges"""
-    
-    #distance between two edges of the pore
-    dist = np.sqrt(np.sum((edgesbottom-edgestop)**2, axis=1))
-    
-    #if the edges are adjacent on y-coordinate - there is no pore
-    nopore = (edgesbottom[:,0]-edgestop[:,0] ==1)
+        #right half of the image
+        rimg = image[:,centerx:]
 
-    #calculate pore radius and set to 0 when there is no pore
-    rpores = np.where(nopore, np.zeros_like(dist), dist) / 2
-
-    return rpores
+        #label clusters on the image
+        rimglabeled, noflabels = label(rimg, structure=s)
+        
+        #inner intersection of vertical midsection and top part of the vesicle
+        innertop = np.max(np.nonzero(rimg[:centery,0]))
+        #define which cluster it belongs to
+        innertoplabel = rimglabeled[innertop, 0]
+        
+        #inner intersection of vertical midsection and bottom part of the vesicle
+        innerbottom = np.min(np.nonzero(rimg[centery:,0])) + centery
+        #define which cluster it belongs to
+        innerbottomlabel = rimglabeled[innerbottom, 0]
+        
+        #if top and bottom is the same cluster - no pore, just putting edges in the center
+        if innertoplabel == innerbottomlabel: 
+            edgestop[i,:] = centery, centerx
+            edgesbottom[i,:] = centery, centerx
+        else:
+            #take only one cluster representing top/bottom part of the vesicle
+            ytop, xtop = np.nonzero(rimglabeled == innertoplabel)
+            ybottom, xbottom = np.nonzero(rimglabeled == innerbottomlabel)
+            #find angles between positive x-dir of horizontal midsection and all points
+            angletop = np.arctan2(centery - ytop, xtop)
+            anglebottom = np.arctan2(centery - ybottom, xbottom)
+            
+            #if bottom and top clusters are angularly overlapping - no pore
+            if max(anglebottom) >= min(angletop):
+                edgestop[i,:] = centery, centerx
+                edgesbottom[i,:] = centery, centerx
+            else:
+                #take the point with minimal angle from the top cluster
+                mintop = np.argmin(angletop)
+                edgestop[i,:] = ytop[mintop], xtop[mintop]+centerx
+                #take the point with maximal angle from the bototm cluster
+                maxbottom = np.argmax(anglebottom)
+                edgesbottom[i,:] = ybottom[maxbottom], xbottom[maxbottom]+centerx 
+                
+    poreradii = np.sqrt(np.sum((edgesbottom-edgestop)**2, axis=1)) / 2
+    return framenos+1, edgestop, edgesbottom, poreradii   
     
 def process_image(filename, nskip):
     """Load image, process it and save results"""
     tif, nofimg = load_imagestack(filename)
-    framenos, edgestop, edgesbottom = pore_edges(tif, nofimg, njump=nskip)
-    rpores = pore_radii(edgestop, edgesbottom)
-    
+    framenos, edgestop, edgesbottom, rpores = pores(tif, nofimg, njump=nskip)
     name, ext = os.path.splitext(filename)
     nameout = name+'_skip%i.txt'%nskip
     out = np.column_stack((rpores, edgestop, edgesbottom, framenos))
